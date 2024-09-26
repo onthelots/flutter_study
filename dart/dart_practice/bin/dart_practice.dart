cbc0cfd0c04cdbfa888515b343bbb86a2cba15c3
iOSDevLime <onthelots@naver.com>
2024-09-26 15:50:41 +0900

    [Add] Practice Dart Variables and Data Type

--- dart/dart_practice/bin/dart_practice.dart
+++ dart/dart_practice/bin/dart_practice.dart
@@ -1,6 +1,14 @@
-
 import 'dart:io';
 
+typedef ListOfInts = List<int>;
+
+ListOfInts reverseListOfNumbers(ListOfInts list) {
+  var reversed = list.reversed;
+  return reversed.toList();
+}
+
+// 실행은, 반드시 main() 함수에서 실시해야 함
 void main() {
-  var numbers = { 1, 2, 3, 4 };
+  var reversedNum = reverseListOfNumbers([1,2,3]);
+  print(reversedNum);
 }
